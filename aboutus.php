a32bf252fd0fab26d00bef899252a46489ddb2d0
Rubeninho <90384253+Rubeninho@users.noreply.github.com>
2022-04-06 16:01:11 +0200

    asdasdasdadadasdasd

--- aboutus.php
+++ aboutus.php
@@ -15,8 +15,9 @@
               <div class="card" style="background-color:white;">
                 <div class="card-body p-5">
                   <h2 class="text-uppercase text-center mb-3 headings">Daniel Hort</h2>
-                    <p class="text-center text-muted mt-3 mb-0">
-                      Nadšenec do komiksů, který při chybových hlášeních programů má chuť procvičit si na počítači údery z karate.
+                  <img src="authors-img/daniel.png" class="images">
+                    <p class="text-center text-muted mt-3 mb-0 paragraphs">
+                      Comics lover, who can damage your body with karate moves, so watch out! Even though he is not in the picture his supportive soul is constantly with us. 
                   </div>
                 </div>
               </div>
@@ -33,9 +34,9 @@
             <div class="card" style="background-color:white;">
               <div class="card-body p-5">
                 <h2 class="text-uppercase text-center mb-3 headings">Filip Edlund</h2>
-                  <p class="text-center text-muted mt-3 mb-0">
-                    Rodák z Horoměřic se švédskými kořeny je jedním z nejobávanějších programátorů na Gymnáziu Arabská. Jeho programátorské smysly se vyjímají mezi ostatními z třídy 3.E.
-            
+                <img src="authors-img/filip.jpg" class="images">
+                  <p class="text-center text-muted mt-3">
+                    Rodák z Horoměřic se švédskými kořeny je jedním z nejobávanějších programátorů na Gymnáziu Arabská. Jeho programátorské smysly se vyjímají mezi ostatními z třídy 3.E. Na obrázku ho můžete vidět, jak svýma vlastníma rukama chytil rybu.
                 </div>
               </div>
             </div>
@@ -52,8 +53,9 @@
           <div class="card" style="background-color:white;">
             <div class="card-body p-5">
               <h2 class="text-uppercase text-center mb-3 headings">Ruben Dobeš</h2>
+              <img src="authors-img/ruben.jpg" class="images">
                 <p class="text-center text-muted mt-3 mb-0">
-                  Ačkoliv tohoto vášnivého fotbalistu nebaví programování, zajímá se o tvorbu webových stránek.
+                  Programming will not ever be his passion, but young footballer will always give maximum for his team and he created web pages for this project. On the picture, he is enjoying summer weather, while playing some mini-golf.
               </div>
             </div>
           </div>
