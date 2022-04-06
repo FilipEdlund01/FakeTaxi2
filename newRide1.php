48b4ddc4110066cc5e11830390c0e3e6367f1781
Rubeninho <90384253+Rubeninho@users.noreply.github.com>
2022-04-06 13:53:29 +0200

    ?DDDDDDDDDDDDDDDDDd

--- newRide1.php
+++ newRide1.php
@@ -1,37 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Register</title>
+	<title>New Ride</title>
 	<meta charset="utf-8">
-	<link rel="stylesheet" href="styles.css" />
+	<link rel="stylesheet" href="css/style.css" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>	
-<body>
-<section class="vh-100 bg-image" style="background-image: url('https://www.autorevue.cz/getthumbnail.aspx?w=1600&h=1000&q=60&id_file=644731428');">
-	<div class="mask d-flex align-items-center h-100 graduate">
+<body class="newRideBackground">
+	<div class="vh-100"> 
+	<div class="mask d-flex align-items-center h-100">
 	  <div class="container h-100">
 		<div class="row d-flex justify-content-center align-items-center h-100">
 		  <div class="col-12 col-md-9 col-lg-7 col-xl-6">
-			<div class="card" style="border-radius: 15px;">
-			  <div class="card-body p-5" >
-				<h2 class="text-uppercase text-center mb-3" style="color: black;">New Ride</h2>
+			<div class="card">
+			  <div class="card-body p-5">
+				<h2 class="text-uppercase text-center mb-3 headings">New Ride</h2>
   
 				<form action="send_data.php" method="post">
   
 				  <div class="form-outline mb-3">
 					<input type="time" id="newRideForm1" class="form-control form-control-lg" name="time" required="required"/>
-					<label class="form-label" for="newRideForm1" style="color: black;">Time</label>
+					<label class="form-label paragraphs" for="newRideForm1" >Time</label>
 				  </div>
 				  
 				  <div class="form-outline mb-3">
 					<input type="date" id="newRideForm2" class="form-control form-control-lg" name="date" required="required"/>
-					<label class="form-label" for="newRideForm2" style="color: black;">Date</label>
+					<label class="form-label paragraphs" for="newRideForm2">Date</label>
 				  </div>
 
 				  <div class="form-outline mb-3">
 					<input type="tel" id="newRideForm3" class="form-control form-control-lg" name="phone" required="required"/>
-					<label class="form-label" for="newRideForm3" style="color: black;">Phone Number</label>
+					<label class="form-label paragraphs" for="newRideForm3">Phone Number</label>
 				  </div>
 				  <a href="newRide2.php"
 				  		type="submit"
@@ -49,6 +49,6 @@
 		</div>
 	  </div>
 	</div>
-  </section>
+	</div>
 </body>
 </html>
