31b28beb0b0a57dc89ab0b9ff5a2912ae661e1b7
Rubeninho <90384253+Rubeninho@users.noreply.github.com>
2022-04-07 22:18:25 +0200

    dasadsadsdas

--- maps.php
+++ maps.php
@@ -1,7 +1,9 @@
 <html>
 
 <head>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
   <link rel="stylesheet" href="css/maps.css" />
+  <link rel="stylesheet" href="css/style.css" />
   <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
     integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
     crossorigin="" />
@@ -11,42 +13,40 @@
 </head>
 
 <body>
+<div class="usermainBackground">
+  <h1 class="mainHeadingMap" style="text-align: center;"> Please if you want to send location copy the current latitude and logitude to the labels below and click Send Location.</h1>
+  <h3 class="headings2" style="text-align: center;">To see the longitude and latitude yoou must click on map first</h3>
+  <h3 class="headings2" style="text-align: center;">Thank you. FAKE-TAXI team.</h3>
 
-  <h1> Please if you want to send location copy the current latitude and logitude to the labels below and click Send Location.</h1>
-  <h2>To see the longitude and latitude yoou must click on map first</h2>
-  <h3>Thank you. FAKE-TAXI team.</h3>
-    
-
-  <p> Current latitude <span id="latitude"></span>.</p>
-  <p> Current longitude <span id="longitude"></span>.</p>
+  <div style="text-align: center; form-outline">
+  <p class="paragraphsMaps" > Current latitude <span id="latitude"></span>.</p>
+  <p class="paragraphsMaps"> Current longitude <span id="longitude"></span>.</p>
+  </div>
 
   <form action="includes/location.inc.php" method="post" id="form">
 
-
-<div class="form-outline">
-  <input type="text" id="latitude" class="form-control form-control-lg" name="latitude" />
-  <label class="form-label" style="color: black;" for="registerForm4">Latitude</label>
+<div style="align-items-center card">
+<div class="form-group" style="text-align:center;">
+  <label class="form-label paragraphsMaps">Latitude</label>
+  <input type="text" id="latitude" class="form-control sizing" name="latitude" />
 </div>
 
-<div class="form-outline mb-2">
-  <input type="text" id="longitude" class="form-control form-control-lg"
-    name="longitude" />
-  <label class="form-label" style="color: black;" for="registerForm5">Longitude</label>
+<div class="form-outline mb-2 col-xs-3" style="text-align:center;">
+<label class="form-label paragraphsMaps">Longitude</label>
+  <input type="text" id="longitude" class="form-control sizing" name="longitude" />
 </div>
 
-
-<div class="form-check d-flex justify-content-center mb-3"></div>
-
-
-<button type="submit" name="submit" href="index.php">Send Location</button>
+<div class="mb-5"></div>
+<a class="btn btn-success btn-block btn-lg graduateButton text-body mb-3 sizing" type="submit" name="submit" href="userMain.php">Send Location</a>
 
   </form>
   <a href='userMain.php'>
-        <button class="GFG">
+        <button class="btn btn-success btn-block btn-lg graduateButton text-body sizing">
             Return Home
         </button>
     </a>
-
+</div>
+    </div>
   <div onload="initializeMaps()" id="map"></div>
 
   <?php
@@ -70,8 +70,3 @@
 </script>
 
 </html>
-
-
-
-
-
